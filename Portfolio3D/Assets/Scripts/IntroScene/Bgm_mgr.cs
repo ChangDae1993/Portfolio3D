a6ae39c7330475be56264f9435b20e2cf345407c
ChangDae1993 <jsj939639@gmail.com>
2023-12-15 10:37:41 +0900

    Wwise Apply_Try2

--- Portfolio3D/Assets/Scripts/IntroScene/Bgm_mgr.cs
+++ Portfolio3D/Assets/Scripts/IntroScene/Bgm_mgr.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Bgm_mgr : MonoBehaviour
 {
-    private void Awake() 
+    private void Awake()
     {
-        var obj = FindObjectsOfType<Bgm_mgr>();
-        if (obj.Length == 1) 
-        { 
-            DontDestroyOnLoad(gameObject); 
-        } 
-        else 
-        { 
-            Destroy(gameObject); 
-        } 
+        //var obj = FindObjectsOfType<Bgm_mgr>();
+        //if (obj.Length == 1) 
+        //{ 
+        //    DontDestroyOnLoad(gameObject); 
+        //} 
+        //else 
+        //{ 
+        //    Destroy(gameObject); 
+        //} 
+
     }
 
+
+    private void Start()
+    {
+
+        AkSoundEngine.PostEvent("BGM_Intro", this.gameObject);
+    }
 }
